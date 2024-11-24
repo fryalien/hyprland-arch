104c9fd1e6a4dbf96a1724559d71d8b470e59fe7
fryalien <164930880+fryalien@users.noreply.github.com>
2024-11-24 13:08:24 +0100

    y

---
+++ copy-dots.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cp -R ~/nix/dots/fish ~/.config/fish
+
+cp -R ~/nix/dots/foot ~/.config/foot
+
+cp -R ~/nix/dots/hypr ~/.config/hypr
+
+cp -R ~/nix/dots/kitty ~/.config/kitty
+
+cp -R ~/nix/dots/konsole ~/.config/konsole
+
+cp -R ~/nix/dots/rofi ~/.config/rofi
+
+cp -R ~/nix/dots/waybar ~/.config/waybar
+
+cp -R ~/nix/dots/doplphinrc ~/.config/
